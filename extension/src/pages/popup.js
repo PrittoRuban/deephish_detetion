47db969ac3bea38a5df8d52715444681410bdfdc
R Pritto Ruban <prittoruban0609@gmail.com>
2025-02-26 19:14:07 +0530

    feat: add Next.js structure with background script and popup page

---
+++ extension/src/pages/popup.js
@@ -0,0 +1,9 @@
+export default function Popup() {
+    return (
+      <main style={{ padding: "20px", width: "300px" }}>
+        <h1>My Chrome Extension</h1>
+        <p>Built with Next.js!</p>
+      </main>
+    );
+  }
+  
