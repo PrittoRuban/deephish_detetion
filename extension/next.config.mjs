a933ece912d1c681bf963c9f24ce95cff7c155ee
R Pritto Ruban <prittoruban0609@gmail.com>
2025-02-26 19:14:32 +0530

    chore: remove background script and popup files; update Next.js config and add new SVG assets

--- extension/next.config.mjs
+++ extension/next.config.mjs
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export", distDir: "out" };
+const nextConfig = {};
 
 export default nextConfig;
--- extension/src/pages/popup.js
+++
@@ -1,9 +0,0 @@
-export default function Popup() {
-    return (
-      <main style={{ padding: "20px", width: "300px" }}>
-        <h1>My Chrome Extension</h1>
-        <p>Built with Next.js!</p>
-      </main>
-    );
-  }
-  
