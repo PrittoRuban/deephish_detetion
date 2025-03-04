f7732114c0998ed1244d0c6701948061cea84f32
R Pritto Ruban <prittoruban0609@gmail.com>
2025-03-04 12:49:12 +0530

    feat: replace Dashboard component with HomeDash; enhance Tailwind CSS configuration and update floating navbar styles

--- dashboard/src/app/page.js
+++ dashboard/src/app/page.js
@@ -1,9 +1,10 @@
-import Dashboard from "@/components/Home";
+import HomeDash from "@/components/HomeDash";
 
 export default async function Home() {
   return (
     <main className="flex flex-col items-center px-4">
-      <Home />
+      <HomeDash />
     </main>
+
   );
 }
--- dashboard/src/components/ui/floating-navbar.jsx
+++ dashboard/src/components/ui/floating-navbar.jsx
@@ -59,11 +59,11 @@ export const FloatingNav = ({ navItems, className }) => {
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden sm:block text-sm">{navItem.name}</span>
+            <span className="hidden sm:block font-semibold text-sm">{navItem.name}</span>
           </Link>
         ))}
         <Link href="/login">
-          <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
+          <button className="border text-sm font-semibold relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
             <span>Login</span>
             <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
           </button>
